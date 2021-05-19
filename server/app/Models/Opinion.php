b434390265b0f3a422fbf7e928339b9c32021f97
darpek123 <darekxprzybysz321@gmail.com>
2021-05-19 12:11:06 +0200

    opinions

---
+++ server/app/Models/Opinion.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Opinion extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'employee_id',
+        'text',
+        'stars',
+    ];
+    use HasFactory;
+}
